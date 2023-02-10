622e5162afd00604493b9a2b2c25a9671335afca
marc <marcgriese76@gmail.com>
2023-02-10 15:16:51 +0100

    Created test.js file

--- src/test.js
+++ src/test.js
@@ -1,29 +1,32 @@
+let obj = [
+    {
+      oct22: 1895368,
+      nov22: 1963413,
+      dec22: 1887455,
+      jan23: 2002944,
+      feb23: 1913215,
+      mar23: 2154969,
+      apr23: 1904758,
+      may23: 2014417,
+      jun23: 1792700,
+      jul23: 1894468,
+      aug23: 1983274,
+      sep23: 2063196
+    }
+  ];
+  
+  let arr = Object.values(obj[0]);
+  
+  //console.log(arr);
+  // Output: [1895368, 1963413, 1887455, 2002944, 1913215, 2154969, 1904758, 2014417, 1792700, 1894468, 1983274, 2063196]
 
-let data = fetch(
-    'http:/localhost:8080/user'
-    ).then((response) => {
-        return response.json()
-    }).then((data) => {
-        return data
-    })
+  let co2MultiplierHungary = 0.26
+  let arrCarbonFootprintHungary = []
 
 
+for (let i = 0; i < arr.length; i++) {
+    arrCarbonFootprintHungary.push(Math.round(arr[i] * co2MultiplierHungary))
+}
 
-console.log(data)
-
-
-
-// console.log(user)
-
-// !async function(){
-//     let data = await fetch('http:/localhost:8080/user')
-//         .then((response) => response.blob())
-//         .then(data => {
-//             return data;
-//         })
-//         .catch(error => {
-//             console.error(error);
-//         });
-    
-//     console.log(data);
-//     }();
+console.log(arrCarbonFootprintHungary)
+  
